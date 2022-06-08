a6d40dc70f7615489b3177dcdb6c2ca00364a045
CelestialMind <64136722+CelestialMind@users.noreply.github.com>
2022-06-08 15:22:42 +0300

    Initial commit

--- app/src/main/java/com/example/noiseboxentree/CustomFragment.java
+++ app/src/main/java/com/example/noiseboxentree/CustomFragment.java
@@ -15,43 +15,8 @@ import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CustomFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CustomFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CustomFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CustomFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CustomFragment newInstance(String param1, String param2) {
-        CustomFragment fragment = new CustomFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     // View elements declaration
     EditText edtCustomPageLength, edtCustomPageWidth, edtCustomPageHeight;
@@ -62,23 +27,29 @@ public class CustomFragment extends Fragment {
     // Database declaration
     DatabaseHelper databaseHelper;
 
+    public CustomFragment() {
+        super(R.layout.fragment_custom);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-        setContentView(R.layout.activity_main_temp);
+    }
 
-        databaseHelper = new DatabaseHelper(this);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View iView = inflater.inflate(R.layout.fragment_custom, container, false);
 
-        initView(); // initializes the activity_main elements
+        initView(iView);
+
+        databaseHelper = new DatabaseHelper(this.getActivity());
+
 
         fABtnOpenCart.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity_temp.this, CartActivity.class);
+            Intent intent = new Intent(CustomFragment.this.getActivity(), CartActivity.class);
             startActivity(intent);
-
         });
 
         // Data retrieving button of the Custom page is clicked
@@ -86,15 +57,16 @@ public class CustomFragment extends Fragment {
             NoiseBox noiseBox = readerFromCustomPage(); // initializes NoiseBox instance using prepared function
             if (null == noiseBox){
                 // when returned from readerFromCustomPage() is null, hence the input was invalid
-                Toast.makeText(this, "Incorrect input", Toast.LENGTH_SHORT).show();
+                Toast.makeText(CustomFragment.this.getContext(), "Incorrect input", Toast.LENGTH_SHORT).show();
             }else{
                 // when input was ok
-                Toast.makeText(this, noiseBox.toString(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(CustomFragment.this.getContext(), noiseBox.toString(), Toast.LENGTH_SHORT).show();
                 if (databaseHelper.addOne(noiseBox)); // finally adds the entered data to the Database
-                else Toast.makeText(this, "But, how?", Toast.LENGTH_SHORT).show(); // to hell with all of it
+                else Toast.makeText(CustomFragment.this.getContext(), "But, how?", Toast.LENGTH_SHORT).show(); // to hell with all of it
             }
         });
 
+        return iView;
     }//---------------------------------------------------------------------------------------------
 
     // Retrieves data from the Custom Page form
@@ -124,20 +96,14 @@ public class CustomFragment extends Fragment {
     }
 
     // initializes the activity_main elements
-    private void initView(){
-        edtCustomPageLength = findViewById(R.id.edtCustomPageLength);
-        edtCustomPageWidth = findViewById(R.id.edtCustomPageWidth);
-        edtCustomPageHeight = findViewById(R.id.edtCustomPageHeight);
-        swCustomPageAdvSp = findViewById(R.id.swCustomPageAdvSp);
-        swCustomPageFan = findViewById(R.id.swCustomPageFan);
-        btnCustomPageAdd = findViewById(R.id.btnCustomPageAdd);
-        fABtnOpenCart = findViewById(R.id.fABtnOpenCart);
+    private void initView(View view){
+        edtCustomPageLength = view.findViewById(R.id.edtCustomPageLength);
+        edtCustomPageWidth = view.findViewById(R.id.edtCustomPageWidth);
+        edtCustomPageHeight = view.findViewById(R.id.edtCustomPageHeight);
+        swCustomPageAdvSp = view.findViewById(R.id.swCustomPageAdvSp);
+        swCustomPageFan = view.findViewById(R.id.swCustomPageFan);
+        btnCustomPageAdd = view.findViewById(R.id.btnCustomPageAdd);
+        fABtnOpenCart = view.findViewById(R.id.fABtnOpenCart);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom, container, false);
-    }
 }
--- app/src/main/java/com/example/noiseboxentree/MainActivity.java
+++ app/src/main/java/com/example/noiseboxentree/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.noiseboxentree;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.example.noiseboxentree.databinding.ActivityMainBinding;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     ViewPager2 viewPager2;
     TabLayout tabLayout;
+    FragmentAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager2 = findViewById(R.id.main_menu_view_pager);
+        viewPager2.setOffscreenPageLimit(2);
         tabLayout = findViewById(R.id.main_menu_tab_layout);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(adapter);
+        tabLayout.addTab(tabLayout.newTab().setText("Catalog page"));
+        tabLayout.addTab(tabLayout.newTab().setText("Custom page"));
+        tabLayout.addTab(tabLayout.newTab().setText("About page"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
     }
 }
